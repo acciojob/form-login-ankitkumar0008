32adab7b2ed416e3e75ca9e43f0005df0f8f102c
ankitkumar0008 <96786356+ankitkumar0008@users.noreply.github.com>
2025-09-24 11:16:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
     //Write your code here
+let form = document.getElementById("form1");
+	let fname = form.fname.value.trim();
+	let lname = form.lname.value.trim();
 
+	alert(`${fname} ${lname}`);
 }
